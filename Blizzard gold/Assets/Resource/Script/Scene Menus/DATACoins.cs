e1124fb66104e52b53fbdc0e0922974ca649eae4
Johnny Pinheiro <pinheiro_dom@hotmail.com.br>
2016-01-25 16:14:12 -0200

    save coins shop

--- Blizzard gold/Assets/Resource/Script/Scene Menus/DATACoins.cs
+++ Blizzard gold/Assets/Resource/Script/Scene Menus/DATACoins.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class DATACoins : MonoBehaviour {
 
     public static int Money;
-    private int MoneyShop;
+    private static int MoneyShop;
+    private int saveMoney;
 
-    // Use this for initialization
-	void Start () {
+    void Awake()
+    {
+        PlayerPrefs.GetInt("moneyShop");
+        saveMoney = PlayerPrefs.GetInt("moneyShop");
         MoneyShop += Money;
-	}
+        Debug.Log(MoneyShop + " dinheiro do jogo");
+    }
+
+    void Start()
+    {
+        if(saveMoney > 0)
+        {
+            MoneyShop += saveMoney;
+        }
+        Debug.Log(MoneyShop + " dinheiro do jogo somado");
+    }
 	
-	// Update is called once per frame
-	void Update () {
-        print(MoneyShop);
-	}
 }
